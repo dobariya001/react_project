a21f56775755510cf3a0e9987b7ceca21827af55
dobariya001 <parthdobariya41@gmail.com>
2024-11-12 16:24:50 +0530

    my first react app

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Navbar />
       <div className="content-container">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/service" element={<Service />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
